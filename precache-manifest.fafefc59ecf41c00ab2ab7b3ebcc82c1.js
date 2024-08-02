0209b3ca8629d4dfe12ca3bd0ca2eee0f658a0f1
You Gao <djx3rn@virginia.edu>
2024-08-01 21:57:56 -0400

    Updates

---
+++ precache-manifest.fafefc59ecf41c00ab2ab7b3ebcc82c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358ca91a39fde4d9eabe3a1c777a4b8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d374373ad70731db4e",
+    "url": "/static/css/main.77c13a1a.chunk.css"
+  },
+  {
+    "revision": "6a50fdc507979ed2cfaf",
+    "url": "/static/js/2.207bcaea.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "e4d374373ad70731db4e",
+    "url": "/static/js/main.775c906b.chunk.js"
+  },
+  {
+    "revision": "e21c8a56d64ede50141d",
+    "url": "/static/js/runtime~main.891e0007.js"
+  }
+]);
