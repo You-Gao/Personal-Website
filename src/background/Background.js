51fdd93d9e66e2ad1036401ae2ae59e694849c05
you.gao@kinsaleins.com <you.gao@kinsaleins.com>
2024-08-02 11:43:13 -0400

    feat: white bg

--- src/background/Background.js
+++ src/background/Background.js
@@ -9,7 +9,7 @@ const Background = () => {
         function init() {
             // Create scene
             scene = new THREE.Scene();
-
+            scene.background = new THREE.Color(0xffffff); // Set background color to black
             // Create camera
             camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
             camera.position.set(0, 10, 0); // Position the camera above the scene
